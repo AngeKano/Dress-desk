6e89327cf59471fca497186a9273f666a1126c24
AngeKano <Angekano002@gmail.com>
2024-01-12 03:21:54 +0000

    VV 12/1/24

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
-export const Login: React.FC = () => {
+export const Login = () => {
 
   const [userEmail, setUserEmail] = useState(String);
   const [userPassword, setUserPassword] = useState(String);
